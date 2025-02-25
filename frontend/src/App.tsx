66a7533690852e80fe3e6d6b7d057d04ea2cb64d
boytur <mask30254@gmail.com>
2025-02-25 16:20:42 +0700

    ✨ feat[App]: simplify imports by removing unused Routes and Route components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./hooks/UseAuth";
 import { AppRoutes } from "./routes/Index";
 import { Toaster } from "@/components/ui/sonner";
