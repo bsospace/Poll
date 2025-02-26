43cdf045031e090e1eff14096a97640b10644e98
Jessada.n <Jessada.n@clicknext.com>
2025-02-26 10:30:42 +0700

    🔧 chore[server]: : Remove unique suffix from multer file name

--- backend/src/utils/multerConfig.util.ts
+++ backend/src/utils/multerConfig.util.ts
@@ -7,8 +7,7 @@ const storage = multer.diskStorage({
     cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, `${file.fieldname}-${uniqueSuffix}${path.extname(file.originalname)}`);
+    cb(null, file.originalname);
   },
 });
 
